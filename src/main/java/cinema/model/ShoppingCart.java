3a76ad1577b6b27f5d9924ac3adc57bcdfc5b2ca
Panchenko Volodymyr <volodymyr.panchenko147@egmail.com>
2022-01-18 21:43:13 +0200

    add spring-part to the project cinema

--- src/main/java/cinema/model/ShoppingCart.java
+++ src/main/java/cinema/model/ShoppingCart.java
@@ -16,13 +16,12 @@ public class ShoppingCart {
     @Id
     private Long id;
     @OneToMany
-    @JoinTable(name = "carts_tickets",
-            joinColumns = @JoinColumn(name = "cart_id"),
+    @JoinTable(name = "shopping_cart_tickets",
+            joinColumns = @JoinColumn(name = "shopping_cart_id"),
             inverseJoinColumns = @JoinColumn(name = "ticket_id"))
     private List<Ticket> tickets;
-    @OneToOne
     @MapsId
-    @JoinColumn(name = "id")
+    @OneToOne
     private User user;
 
     public Long getId() {
@@ -51,7 +50,9 @@ public class ShoppingCart {
 
     @Override
     public String toString() {
-        return "ShoppingCart{" + "id=" + id + ", tickets="
-                + tickets + ", user=" + user + '}';
+        return "ShoppingCart{"
+                + "id=" + id
+                + ", tickets=" + tickets
+                + ", user=" + user + '}';
     }
 }
